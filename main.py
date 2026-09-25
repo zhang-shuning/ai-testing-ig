bc35ac27ad994109ef394a9598bf7eba47ea8157
zhang-shuning <ningnongidk@gmail.com>
2026-09-24 20:43:35 -0700

    Now clears metadata of image.

--- main.py
+++ main.py
@@ -5,18 +5,29 @@ This code was/is copy and pasted boiler plate
 import os
 import base64
 from openrouter import OpenRouter
+import io
+from PIL import Image
 
 RUN_AI = True
 
 def encode_image(path):
-    with open(path, "rb") as f:
-        return base64.b64encode(f.read()).decode("utf-8")
+    #AI generated function
+    #Clears metadata
+    with Image.open(path) as img:
+        with io.BytesIO() as buffer:
+            img.getexif().clear()
+            img.save(buffer, format="JPEG")
+            return base64.b64encode(buffer.getvalue()).decode("utf-8")
 
-image = r"C:\Users\user\Pictures\tank.png"
+image = r"C:\Users\user\Desktop\Python\ai-testing-ig\images_to_use\Abyssinian_1.jpg"
 base64_image = encode_image(image)
 data_url = f"data:image/jpeg;base64,{base64_image}"
 
 messages = [
+    {
+        "role": "system",
+        "content": "You will be shown an image of a cat or dog, your job is to respond with only the breed of the animal with no capitals."
+    },
     {
         "role": "user",
         "content": [
@@ -35,6 +46,7 @@ messages = [
 ]
 
 if __name__ == "__main__":
+    print(len(base64_image))
     if RUN_AI:
         client = OpenRouter(
             api_key = os.environ["API_KEY"],
